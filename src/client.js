cf67babeec743f7e58ecc673656e20ad66b275fe
FoLZer <nikolaenko.gleb.044@gmail.com>
2021-08-01 18:18:48 +0300

    Remove message listener instead of deleting it later

--- src/client.js
+++ src/client.js
@@ -61,7 +61,6 @@ class CommandoClient extends discord.Client {
 
 		// Set up command handling
 		const msgErr = err => { this.emit('error', err); };
-		this.on('message', message => { this.dispatcher.handleMessage(message).catch(msgErr); });
 		this.on('messageUpdate', (oldMessage, newMessage) => {
 			this.dispatcher.handleMessage(newMessage, oldMessage).catch(msgErr);
 		});
